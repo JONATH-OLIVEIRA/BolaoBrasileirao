d09a086a54ce1c0e5888b4831ea04ee2d724daf9
Jonath Oliveira <akiles22a@gmail.com>
2025-05-07 16:24:21 -0300

    criacao projeto

---
+++ apostas/src/main/java/br/com/apostas/ApostasBrasileiraoApplication.java
@@ -0,0 +1,13 @@
+package br.com.apostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApostasBrasileiraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApostasBrasileiraoApplication.class, args);
+	}
+
+}
