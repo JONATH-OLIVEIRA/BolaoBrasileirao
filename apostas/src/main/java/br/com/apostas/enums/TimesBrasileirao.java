665f75089a2b350737f44931153d5d625a88ee16
Jonath Oliveira <akiles22a@gmail.com>
2025-05-08 11:12:48 -0300

    criacao da classe partadia e configuracao da logica

---
+++ apostas/src/main/java/br/com/apostas/enums/TimesBrasileirao.java
@@ -0,0 +1,24 @@
+package br.com.apostas.enums;
+
+public enum TimesBrasileirao {
+    ATLETICO_MG,
+    BAHIA,
+    BOTAFOGO,
+    CEARA,
+    CORINTHIANS,
+    CRUZEIRO,
+    FLAMENGO,
+    FLUMINENSE,
+    FORTALEZA,
+    GREMIO,
+    INTERNACIONAL,
+    JUVENTUDE,
+    MIRASSOL,
+    PALMEIRAS,
+    RED_BULL_BRAGANTINO,
+    SANTOS,
+    SAO_PAULO,
+    SPORT,
+    VASCO_DA_GAMA,
+    VITORIA;
+}
--- apostas/src/main/java/br/com/apostas/model/Partida.java
+++ apostas/src/main/java/br/com/apostas/model/Partida.java
@@ -2,121 +2,115 @@ package br.com.apostas.model;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Objects;    
+import java.util.Objects;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 
 import br.com.apostas.enums.ResultadoPartida;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import br.com.apostas.enums.TimesBrasileirao;
 
 @Entity
 @Table(name = "partidas")
 public class Partida implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @NotNull(message = "Rodada é obrigatória")
-    private Integer rodada;
+	@NotNull(message = "Rodada é obrigatória")
+	private Integer rodada;
 
-    @NotBlank(message = "Nome do time da casa não pode estar vazio")
-    private String timeCasa;
+	@Enumerated(EnumType.STRING)
+	@NotNull(message = "Time da casa é obrigatório")
+	private TimesBrasileirao timeCasa;
 
-    @NotBlank(message = "Nome do time visitante não pode estar vazio")
-    private String timeFora;
+	@Enumerated(EnumType.STRING)
+	@NotNull(message = "Time visitante é obrigatório")
+	private TimesBrasileirao timeFora;
 
-    @Enumerated(EnumType.STRING)
-    private ResultadoPartida resultado; // CASA, FORA, EMPATE
+	@Enumerated(EnumType.STRING)
+	@NotNull(message = "Resultado da partida é obrigatório")
+	private ResultadoPartida resultado; // CASA, EMPATE, FORA
 
-    @OneToMany(mappedBy = "partida", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Aposta> apostas;
+	@OneToMany(mappedBy = "partida", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Aposta> apostas;
 
-    // Construtores
-    public Partida() {}
+	// Construtor padrão
+	public Partida() {
+	}
 
-    public Partida(Long id, Integer rodada, String timeCasa, String timeFora, ResultadoPartida resultado) {
-        this.id = id;
-        this.rodada = rodada;
-        this.timeCasa = timeCasa;
-        this.timeFora = timeFora;
-        this.resultado = resultado;
-    }
+	// Construtor personalizado
+	public Partida(Integer rodada, TimesBrasileirao timeCasa, TimesBrasileirao timeFora, ResultadoPartida resultado) {
+		this.rodada = rodada;
+		this.timeCasa = timeCasa;
+		this.timeFora = timeFora;
+		this.resultado = resultado;
+	}
+	
+	// Getters e Setters
+	public Long getId() {
+		return id;
+	}
 
-    // Getters e Setters
-    public Long getId() {
-        return id;
-    }
+	public Integer getRodada() {
+		return rodada;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public TimesBrasileirao getTimeCasa() {
+		return timeCasa;
+	}
 
-    public Integer getRodada() {
-        return rodada;
-    }
+	public TimesBrasileirao getTimeFora() {
+		return timeFora;
+	}
 
-    public void setRodada(Integer rodada) {
-        this.rodada = rodada;
-    }
+	public ResultadoPartida getResultado() {
+		return resultado;
+	}
 
-    public String getTimeCasa() {
-        return timeCasa;
-    }
+	public List<Aposta> getApostas() {
+		return apostas;
+	}
 
-    public void setTimeCasa(String timeCasa) {
-        this.timeCasa = timeCasa;
-    }
+	public void setRodada(Integer rodada) {
+		this.rodada = rodada;
+	}
 
-    public String getTimeFora() {
-        return timeFora;
-    }
+	public void setTimeCasa(TimesBrasileirao timeCasa) {
+		this.timeCasa = timeCasa;
+	}
 
-    public void setTimeFora(String timeFora) {
-        this.timeFora = timeFora;
-    }
+	public void setTimeFora(TimesBrasileirao timeFora) {
+		this.timeFora = timeFora;
+	}
 
-    public ResultadoPartida getResultado() {
-        return resultado;
-    }
+	public void setResultado(ResultadoPartida resultado) {
+		this.resultado = resultado;
+	}
 
-    public void setResultado(ResultadoPartida resultado) {
-        this.resultado = resultado;
-    }
+	public void setApostas(List<Aposta> apostas) {
+		this.apostas = apostas;
+	}
 
-    public List<Aposta> getApostas() {
-        return apostas;
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 
-    public void setApostas(List<Aposta> apostas) {
-        this.apostas = apostas;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null || getClass() != obj.getClass())
+			return false;
+		Partida other = (Partida) obj;
+		return Objects.equals(id, other.id);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Partida other = (Partida) obj;
-        return Objects.equals(id, other.id);
-    }
-
-    @Override
-    public String toString() {
-        return "Partida [id=" + id + ", rodada=" + rodada + ", timeCasa=" + timeCasa +
-               ", timeFora=" + timeFora + ", resultado=" + resultado + "]";
-    }
+	@Override
+	public String toString() {
+		return "Partida [id=" + id + ", rodada=" + rodada + ", timeCasa=" + timeCasa + ", timeFora=" + timeFora
+				+ ", resultado=" + resultado + "]";
+	}
 }
